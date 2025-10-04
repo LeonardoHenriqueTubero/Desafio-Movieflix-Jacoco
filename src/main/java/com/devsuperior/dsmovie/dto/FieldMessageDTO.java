55a90c863b8ffd3b2d91188c27c0a39a1080ae89
Leoanrdo <leonardohenriquetubero@gmail.com>
2025-10-03 21:58:56 -0300

    Desafio corrigido

--- src/main/java/com/devsuperior/dsmovie/dto/FieldMessageDTO.java
+++ src/main/java/com/devsuperior/dsmovie/dto/FieldMessageDTO.java
@@ -5,6 +5,8 @@ public class FieldMessageDTO {
 	private String fieldName;
     private String message;
 
+    public FieldMessageDTO() {}
+
     public FieldMessageDTO(String fieldName, String message) {
         this.fieldName = fieldName;
         this.message = message;
